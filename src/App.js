509751c4b25c1e21e9c7c6e2c5a6dd28804d6091
ela2002 <elarebai@gmail.com>
2024-02-23 12:21:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/authContext";
+import Home from "./components/home/Home";
+import Login from "./components/auth/login";
+import Onboard from "./components/onboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <div className="w-full h-screen flex flex-col">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/onboard" element={<Onboard />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
